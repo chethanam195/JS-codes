541526fbd7d28b200abb4bfbd1c1f86ff707d4ab
chethanam195 <chethana.m195@gmail.com>
2026-02-05 11:48:14 +0530

    Add conversion_operations.js with JavaScript examples
    This file contains various JavaScript operations and type conversions, including examples of increment operators and string concatenation.

---
+++ conversion_operations.js
@@ -0,0 +1,59 @@
+let score=undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueNumber=Number(score);
+// console.log(typeof (valueNumber));
+// console.log(valueNumber);
+
+// "33" =. 33
+//"df45" => NAN
+// true => 1; false=>0;
+// "chethana" => true
+
+// let isloggedIn=1
+// let boolenIsLoggedIn=Boolean(isloggedIn)
+// console.log(boolenIsLoggedIn);
+
+// let someNumber = 33
+// let stringNumber=String(someNumber);
+// console.log(typeof stringNumber)
+
+
+/////////Operations////////////////////
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+
+let str1="chethana"
+let str2=" successful"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1"+2); output:12
+// console.log(1+"2"); output:12
+// console.log("1"+ 2 + 2); output:122
+// console.log(1+2+"2"); output:32
+// console.log((3+4) * 5 % 3); output:2
+//console.log(+true)
+//console.log(+"")
+let num1,num2,num3
+num1=num2=num3=2+2 // in these form should be not written
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
+
+// postfix increment
+let x=3;
+const y=x++;
+console.log(x);
+console.log(y);
+
+// prefix increment
+let x1=3
+let y2=++x1;
+console.log(x1);
+console.log(y2);
