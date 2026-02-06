131d27f700f76090554ffde32ffe1f3eb364f61b
chethanam195 <chethana.m195@gmail.com>
2026-02-06 14:19:01 +0530

    Add random number generation between min and max

---
+++ num_and_math.js
@@ -0,0 +1,24 @@
+// // // const score=344
+// // // const balance=new Number(344)
+// // // console.log(score)
+// // // console.log(balance)
+// // // const name=new String("chethana")
+// // // console.log(name)
+// // // console.log(balance.toString().length)
+// // // console.log(balance.toFixed(4))
+// // // const otherNumber=78.990
+// // // console.log(otherNumber.toPrecision(3))
+
+// // const hundreds=234456777
+// // //console.log(hundreds.toLocaleString('en-IN'));
+// // //+++++++++++++++++++++++++++++++++++++++++++//
+// // console.log(Math)
+// // console.log(Math.abs(-4))
+// // console.log(Math.round(3.4))
+// // console.log(Math.sqrt(25))
+// // console.log(Math.sin(3))
+
+// console.log((Math.random()*10)+1
+let min=10
+let max=45
+console.log(Math.floor(Math.random()*(max-min+1))+min)
