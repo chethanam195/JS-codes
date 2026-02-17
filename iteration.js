6fa85d677e652ef3096d3587764b674a593194d9
chethanam195 <chethana.m195@gmail.com>
2026-02-17 13:56:03 +0530

    Implement for loop examples in iteration.js
    Added various for loop examples demonstrating iteration and control statements.

---
+++ iteration.js
@@ -0,0 +1,43 @@
+// for loop 
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element==5){
+        //console.log("5 is the best number")
+    }
+    //console.log(element)
+}
+
+for (let i = 0; i <10; i++) {
+    //console.log(`outer loop value${i}`)
+   for (let j = 1;j < 10;j++) {
+    //console.log(`Inner the loop${j} and the inner loop${i}`)
+  //console.log(i+'*' + j+'='+ i*j)
+    
+   }
+    
+}
+
+let myarray=["flash","Superman","Batman"]
+//console.log(myarray.length)
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    //console.log(element)
+    
+}
+
+// on condition we should follow there is two break and continue
+// for (let index = 1; index <=20; index++) {
+//     if (index==5){
+//         console.log(`Detected 5`)
+//         break
+//     }
+//     console.log(`value of i is ${index}`)
+// }
+for (let index = 1; index <=20; index++) {
+    if (index==5){
+        console.log(`Detected 5`)
+        continue
+    }
+    console.log(`value of i is ${index}`)
+}
