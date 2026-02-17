0efe6f54295aafc7a38313722700e0acf6ff7cee
chethanam195 <chethana.m195@gmail.com>
2026-02-17 20:05:55 +0530

    Add object and loop examples in object.js

---
+++ object.js
@@ -0,0 +1,20 @@
+const myObject={
+    js:'javascript',
+    cpp:"c++",
+    rb:"ruby",
+    swift:"swift by apple"
+}
+for (const key in myObject) {
+   console.log(`${key} shortcut is for ${myObject[key]}`)
+    
+    
+}
+
+const programming=["python","java","C"]
+for (const key in programming) {
+   console.log(programming[key])
+    
+    
+}
+// for of loop where we use arrays
+// for in loop we are use object
